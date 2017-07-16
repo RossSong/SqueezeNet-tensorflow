414a7ddc623ddcdb92391f97a6a5440e36fac038
TropComplique <antoshchenko.b@gmail.com>
2017-07-16 09:27:00 +0300

    a lot of changes

--- nn_squeeze_net.py
+++ nn_squeeze_net.py
@@ -8,7 +8,15 @@ from nn_parts import _mapping, _add_weight_decay
 
 
 class SqueezeNet:
-    def __init__(self, num_classes, optimizer, weight_decay=None):
+    def __init__(self, optimizer, num_classes=1000, weight_decay=None):
+        """Create the SqueezeNet computational graph.
+
+        Arguments:
+            optimizer: A Tensorflow optimizer.
+            num_classes: An integer.
+            weight_decay: A scalar or None.
+        """
+
         self.graph = tf.Graph()
         with self.graph.as_default():
 
@@ -57,8 +65,37 @@ class SqueezeNet:
         self.graph.finalize()
 
     def fit(self, run, X_train, Y_train, X_test, Y_test,
-            batch_size, num_epochs, validation_step, patience,
-            initial_weights, warm=False, verbose=True):
+            batch_size, num_epochs, validation_step, patience=10,
+            initial_weights=None, warm=False, verbose=True):
+        """Fit the defined network.
+
+        Arguments:
+            run: An integer that determines a folder where logs and the fitted model
+                will be saved.
+            X_train: A numpy array of shape [n_train_samples, n_features]
+                and of type 'float32'.
+            Y_train: A numpy array of shape [n_train_samples, n_classes]
+                and of type 'float32'.
+            X_test: A numpy array of shape [n_test_samples, n_features]
+                and of type 'float32'.
+            Y_test: A numpy array of shape [n_test_samples, n_classes]
+                and of type 'float32'.
+            batch_size: An integer.
+            num_epochs: An integer.
+            validation_step: An integer, establishes the step when train and test
+                logloss/accuracy will be computed and shown.
+            patience: An integer, number of validation steps before early stopping if
+                test logloss isn't improving.
+            initial_weights: A dictionary of weights to initialize network with.
+            warm: Boolean, if `True` then resume training from the previously
+                saved model.
+            verbose: Boolean, whether to print train and test logloss/accuracy
+                during fitting.
+
+        Returns:
+            losses: A list of tuples containing train and test logloss/accuracy.
+            is_early_stopped: Boolean, if `True` then fitting is stopped early.
+        """
 
         dir_to_log = 'logs/run' + str(run)
         dir_to_save = 'saved/run' + str(run)
@@ -81,14 +118,18 @@ class SqueezeNet:
 
         if not warm:
             sess.run(self.init)
-            for w in initial_weights:
-                op = self.assign_weights[w]
-                sess.run(op, {'utilities/' + w: initial_weights[w]})
+            if initial_weights is not None:
+                for w in initial_weights:
+                    op = self.assign_weights[w]
+                    sess.run(op, {'utilities/' + w: initial_weights[w]})
         else:
             self.saver.restore(sess, dir_to_save + '/model')
 
         num_batches = int(len(X_train)/batch_size)
         losses = []
+        self.running_loss = 0.0
+        self.running_accuracy = 0.0
+        self.validation_step = validation_step
         is_early_stopped = False
         self.steps_trained = 0 if not warm else self.steps_trained
         training_steps = range(
@@ -102,10 +143,14 @@ class SqueezeNet:
         self.start = time.time()
         for step in training_steps:
             if (step % validation_step != 0):
-                sess.run(self.optimize)
+                _, batch_loss, batch_accuracy = sess.run(
+                    [self.optimize, self.log_loss, self.accuracy]
+                )
+                self.running_loss += batch_loss
+                self.running_accuracy += batch_accuracy
             else:
                 losses += [self._get_summaries(
-                    step, sess, X_train, Y_train, X_test, Y_test,
+                    step, sess, X_test, Y_test,
                     batch_size, num_batches, verbose
                 )]
                 if _is_early_stopping(losses, patience, 1):
@@ -122,54 +167,71 @@ class SqueezeNet:
 
         return losses, is_early_stopped
 
-    def predict_proba(self, X):
+    def predict_proba(self, X, network_weights=None):
+        """Predict classes with the fitted model.
+
+        Arguments:
+            X: A numpy array of shape [n_samples, n_features]
+                and of type 'float32'.
+            network_weights: A dictionary of weights to initialize
+                network with or None.
+
+        Returns:
+            predictions: A numpy array of shape [n_samples, n_classes]
+                and of type 'float32'.
+        """
         sess = tf.Session(graph=self.graph)
-        self.saver.restore(sess, 'saved/run' + str(self.run) + '/model')
+
+        if network_weights is not None:
+            for w in network_weights:
+                op = self.assign_weights[w]
+                sess.run(op, {'utilities/' + w: network_weights[w]})
+        else:
+            self.saver.restore(sess, 'saved/run' + str(self.run) + '/model')
+
         feed_dict = {'inputs/X:0': X, 'control/is_training:0': False}
         predictions = sess.run(self.predictions, feed_dict)
         sess.close()
         return predictions
 
-    def _get_summaries(self, step, sess, X_train, Y_train,
-                       X_test, Y_test, batch_size, num_batches, verbose):
+    def _get_summaries(self, step, sess, X_test, Y_test, num_batches, verbose):
 
-            entry = '{0:.2f}'.format(step/num_batches)
-            run_options = tf.RunOptions(
-                trace_level=tf.RunOptions.FULL_TRACE
-            )
-            run_metadata = tf.RunMetadata()
-            _, train_summary, train_grad_summary = sess.run(
-                [self.optimize, self.merged, self.grad_summaries],
-                options=run_options, run_metadata=run_metadata
-            )
-            self.writer.add_run_metadata(run_metadata, entry)
-            self.writer.add_summary(train_summary, step)
-            self.writer.add_summary(train_grad_summary, step)
+        entry = '{0:.2f}'.format(step/num_batches)
+        run_options = tf.RunOptions(
+            trace_level=tf.RunOptions.FULL_TRACE
+        )
+        run_metadata = tf.RunMetadata()
+        _, batch_loss, batch_accuracy, train_summary, train_grad_summary = sess.run(
+            [self.optimize, self.log_loss, self.accuracy, self.merged, self.grad_summaries],
+            options=run_options, run_metadata=run_metadata
+        )
+        self.writer.add_run_metadata(run_metadata, entry)
+        self.writer.add_summary(train_summary, step)
+        self.writer.add_summary(train_grad_summary, step)
+        self.running_loss += batch_loss
+        self.running_accuracy += batch_accuracy
 
-            choice = np.random.choice(
-                np.arange(0, len(X_train)),
-                size=8*batch_size, replace=False
-            )
+        rows = np.random.choice(
+            np.arange(0, len(X_test)),
+            size=1024, replace=False
+        )
+        feed_dict_test = {
+            'inputs/X:0': X_test[rows],
+            'inputs/Y:0': Y_test[rows],
+            'control/is_training:0': False
+        }
+        test_loss, test_accuracy = sess.run([self.log_loss, self.accuracy], feed_dict_test)
 
-            feed_dict_train = {
-                'inputs/X:0': X_train[choice],
-                'inputs/Y:0': Y_train[choice],
-                'control/is_training:0': False
-            }
-            feed_dict_test = {
-                'inputs/X:0': X_test,
-                'inputs/Y:0': Y_test,
-                'control/is_training:0': False
-            }
-            score_ops = [self.log_loss, self.accuracy]
-            train_loss, train_accuracy = sess.run(score_ops, feed_dict_train)
-            test_loss, test_accuracy = sess.run(score_ops, feed_dict_test)
-            end = time.time()
+        train_loss = self.running_loss/self.validation_step
+        train_accuracy = self.running_accuracy/self.validation_step
+        end = time.time()
 
-            if verbose:
-                print('{0}  {1:.3f} {2:.3f} {3:.3f} {4:.3f}  {5:.3f}'.format(
-                    entry, train_loss, test_loss,
-                    train_accuracy, test_accuracy, end - self.start
-                ))
+        if verbose:
+            print('{0}  {1:.3f} {2:.3f} {3:.3f} {4:.3f}  {5:.3f}'.format(
+                entry, train_loss, test_loss,
+                train_accuracy, test_accuracy, end - self.start
+            ))
 
-            return train_loss, test_loss
+        self.running_loss = 0.0
+        self.running_accuracy = 0.0
+        return train_loss, test_loss, train_accuracy, test_accuracy
